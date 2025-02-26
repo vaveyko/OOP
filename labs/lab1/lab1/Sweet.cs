864058456f52358b6a0173326d16e84cbc0d17be
vaveyko <zakhvey.ivan@gmail.com>
2025-02-26 23:22:56 +0300

    add constructor to Sweet class

--- labs/lab1/lab1/Sweet.cs
+++ labs/lab1/lab1/Sweet.cs
@@ -2,5 +2,15 @@ namespace lab1;
 
 public abstract class Sweet : Sweetness
 {
-    public string taste { get; protected set; }
+    public string Taste
+    {
+        get => Taste;
+        protected set => Taste = string.IsNullOrWhiteSpace(value) ? "--" : value;
+    }
+
+    protected Sweet(int weigth, int sweetPercent, string companiName = "", string taste="")
+        : base(weigth, sweetPercent, companiName)
+    {
+        this.Taste = taste;
+    }
 }

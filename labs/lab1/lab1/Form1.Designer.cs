8316506572ccaaf8416a46a6465efe312baa28ac
vaveyko <zakhvey.ivan@gmail.com>
2025-02-27 22:08:52 +0300

    add createElemCard method and design

--- labs/lab1/lab1/Form1.Designer.cs
+++ labs/lab1/lab1/Form1.Designer.cs
@@ -24,16 +24,48 @@ partial class Form1
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        ElementPanel = new System.Windows.Forms.FlowLayoutPanel();
+        CountLabel = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // ElementPanel
+        // 
+        ElementPanel.AutoScroll = true;
+        ElementPanel.Location = new System.Drawing.Point(36, 48);
+        ElementPanel.Name = "ElementPanel";
+        ElementPanel.Size = new System.Drawing.Size(853, 437);
+        ElementPanel.TabIndex = 0;
+        ElementPanel.WrapContents = false;
+        // 
+        // CountLabel
+        // 
+        CountLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+        CountLabel.Location = new System.Drawing.Point(382, 4);
+        CountLabel.Name = "CountLabel";
+        CountLabel.Size = new System.Drawing.Size(150, 41);
+        CountLabel.TabIndex = 1;
+        CountLabel.Text = "0";
+        CountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(923, 648);
+        Controls.Add(CountLabel);
+        Controls.Add(ElementPanel);
+        Text = "Some interesting stuff";
+        ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Label CountLabel;
+
+    private System.Windows.Forms.FlowLayoutPanel ElementPanel;
 
     #endregion
 }
--- labs/lab1/lab1/Form1.cs
+++ labs/lab1/lab1/Form1.cs
@@ -2,8 +2,70 @@ namespace lab1;
 
 public partial class Form1 : Form
 {
+    Dictionary<int, string> fileNames = new Dictionary<int, string>()
+    {
+        {1, "images/chupa-chups.jpg"},
+        {2, "images/bolci.jpg"},
+        {3, "images/alenka.jpg"},
+        {4, "images/guyLian.jpg"},
+        {5, "images/tinki.jpg"},
+    };
+    private void CreateElemCard(Sweetness sweet, int fileIndex)
+    {
+        // Создание панели для изображения и подписи
+        Panel panel = new Panel();
+        panel.Size = new Size(300, 400);
+        panel.BackColor = Color.FromArgb(230, 230, 230);
+
+        // Создание PictureBox
+        PictureBox pictureBox = new PictureBox();
+        pictureBox.Size = new Size(300, 300);
+        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        pictureBox.Image = Image.FromFile(fileNames[fileIndex]); // Путь к изображению
+            
+            
+        // Создание Label1
+        Label label1 = new Label();
+        label1.Text = "Compani: " + sweet.CompaniName;
+        label1.TextAlign = ContentAlignment.MiddleCenter;
+        label1.AutoSize = true;
+        label1.Location = new Point(10, 310); // Положение внутри панели
+        // Создание Label2
+        Label label2 = new Label();
+        label2.Text = "Weight: " + sweet.Weight.ToString() + " g.";
+        label2.TextAlign = ContentAlignment.MiddleCenter;
+        label2.AutoSize = true;
+        label2.Location = new Point(10, 340);
+        // Создание Label3
+        Label label3 = new Label();
+        label3.Text = "Sweetness: " + sweet.SweetPercent.ToString() + " %";
+        label3.TextAlign = ContentAlignment.MiddleCenter;
+        label3.AutoSize = true;
+        label3.Location = new Point(10, 370);
+
+        // Добавление PictureBox и Label в панель
+        panel.Controls.Add(pictureBox);
+        panel.Controls.Add(label1);
+        panel.Controls.Add(label2);
+        panel.Controls.Add(label3);
+
+        // Добавление панели в FlowLayoutPanel
+        ElementPanel.Controls.Add(panel);
+        CountLabel.Text = sweet.Count.ToString();
+    }
     public Form1()
     {
         InitializeComponent();
+
+        Sweetness sweet = new Candy(100, 23, "Chupa-Chups");
+        CreateElemCard(sweet, 1);
+        sweet = new FillingChocolate(1045, 1233, "Bolci");
+        CreateElemCard(sweet, 2);
+        sweet = new Chocolate(230, 83, "Alenka");
+        CreateElemCard(sweet, 3);
+        sweet = new ChocolateCandy(400, 93, "GuyLian");
+        CreateElemCard(sweet, 4);
+        sweet = new Marmalade(120, 67, 12,"Tinki");
+        CreateElemCard(sweet, 5);
     }
 }
